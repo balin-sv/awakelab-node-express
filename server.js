060dee5ecac287793f2bc46c9909ba4fde555e8a
balin-sv <balinsvet@gmail.com>
2022-09-28 17:12:45 -0300

    :feat: add sum logig

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const getGastos = require("./getGastos");
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://localhost:3000/",
   })
 );
-
-async function getRundomUser() {
-  return await axios.get("https://randomuser.me/api/");
-}
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.json());
+
+async function getRundomUser() {
+  return await axios.get("https://randomuser.me/api/");
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -35,7 +35,6 @@ app.post("/roommate", (req, res) => {
       id: uuidv1(),
       name: newRoomme,
     };
-
     try {
       if (!fs.existsSync("public/roommates.json")) {
         fs.writeFileSync(
@@ -71,8 +70,10 @@ app.post("/roommate", (req, res) => {
 });
 
 app.post("/gasto", (req, res) => {
-  let payload = req.body;
-  let newObj = { ...payload, id: uuidv1() };
+  console.log(req.body);
+  let json;
+  let total;
+  let newObj = { ...req.body, id: uuidv1() };
   try {
     if (!fs.existsSync("public/gastos.json")) {
       fs.writeFileSync(
@@ -89,14 +90,38 @@ app.post("/gasto", (req, res) => {
             "utf8"
           );
         } else {
-          let json = JSON.parse(data);
+          json = JSON.parse(data);
           json.push(newObj);
+          total = json.reduce((sum, { monto }) => sum + monto, 0);
+
           fs.writeFileSync(
             "public/gastos.json",
             JSON.stringify(json, null, 2),
             "utf8"
           );
         }
+      });
+      fs.readFile("public/roommates.json", (err, data) => {
+        let arr = JSON.parse(data);
+        let debe = total / arr.length;
+        let aux2 = arr.map((item) => {
+          if (item.id == newObj.uid) {
+            if (item.hasOwnProperty("recibe")) {
+              let totalRecibe = item.recibe + newObj.monto;
+              console.log(totalRecibe);
+              return { ...item, recibe: totalRecibe, debe };
+            } else {
+              return { ...item, recibe: newObj.monto, debe };
+            }
+          } else {
+            return { ...item, debe };
+          }
+        });
+        fs.writeFileSync(
+          "public/roommates.json",
+          JSON.stringify(aux2, null, 2),
+          "utf8"
+        );
       });
     }
     console.log("Data successfully saved to disk");
@@ -130,27 +155,56 @@ app.delete("/gasto", (req, res) => {
       "utf8"
     );
   });
+
   res.sendStatus(200);
 });
 
 app.put("/gasto", (req, res) => {
+  let aux;
+  let total;
   let payload = req.body;
   let newObj = { ...payload, id: uuidv1() };
   fs.readFile("public/gastos.json", (err, data) => {
     let arr = JSON.parse(data);
-    let aux = arr.map((item) => {
+    aux = arr.map((item) => {
       if (item.id == req.query.id) {
         return newObj;
       } else {
         return item;
       }
     });
+    total = aux.reduce((sum, { monto }) => sum + monto, 0);
+
     fs.writeFileSync(
       "public/gastos.json",
       JSON.stringify(aux, null, 2),
       "utf8"
     );
+
+    fs.readFile("public/roommates.json", (err, data) => {
+      let arr = JSON.parse(data);
+      let debe = total / arr.length;
+      let aux2 = arr.map((item) => {
+        if (item.id == newObj.uid) {
+          if (item.hasOwnProperty("recibe")) {
+            let totalRecibe = item.recibe + newObj.monto;
+            console.log(totalRecibe);
+            return { ...item, recibe: totalRecibe, debe };
+          } else {
+            return { ...item, recibe: newObj.monto, debe };
+          }
+        } else {
+          return { ...item, debe };
+        }
+      });
+      fs.writeFileSync(
+        "public/roommates.json",
+        JSON.stringify(aux2, null, 2),
+        "utf8"
+      );
+    });
   });
+
   res.sendStatus(200);
 });
 
